7a862e30b5b1c89cdfa826a4fa3ce95043d9cd3f
ridasadiki <mohamedridasadiki1@gmail.com>
2023-09-20 10:27:20 -0700

    simple shell

---
+++ exuctecom.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+int _executecom(char **commande, char **ar);
+{
+
+	pid_t chil;
+	int statu;
+
+	chil = fork();
+	if (chil == 0)
+	{
+		if (execve(commande[0], commande, envir) == -1)
+		{
+			perror(ar[0]);
+			freearrforstrings(commande);
+			exit(0);
+		}
+	}
+	else
+	{
+		waitpid(chil, &statu, 0);
+		freearrforstrings(commande);
+	}
+	return(WEXITSTATUS(statu))
+}
--- splitcom.c
+++ splitcom.c
@@ -7,13 +7,13 @@ char **splitcom(char *ligne)
 	char *split = NULL, *tmpp = NULL;
 	char **commande = NULL;
 	int cpteur = 0, j = 0;
-	
+
 	if (!ligne)
-		return(NULL);
+		return (NULL);
 	tmpp = _strdup(ligne);
 
 	split = strtok(tmpp, DELM); /*DELIM: DELM (\t\n) il faut etre declare en shell.h*/
-	
+
 	if (split == NULL)
 	{
 		free(ligne); ligne = NULL;
@@ -38,11 +38,11 @@ char **splitcom(char *ligne)
 	split = strtok(ligne, DELM);
 	while (split)
 	{
-		commande[j]= _strdup(split);
+		commande[j] = _strdup(split);
 		split = strtok(NULL, DELM);
 		j++;
 	}
 	free(ligne), ligne = NULL;
-	commande[j] =NULL;
+	commande[j] = NULL;
 	return (commande);
 }
